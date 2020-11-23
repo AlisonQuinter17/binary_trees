2f41572de3fa2c5f9075dda0db2ab44fc8179fbe
Athesto <gamez.live@gmail.com>
2020-11-23 15:37:06 -0500

    0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - ?
+ * @parent: ?
+ * @value: ?
+ * Return: ?
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	(void) parent;
+	(void) value;
+	return (NULL);
+}
