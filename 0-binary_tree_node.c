92aff3d92a47bd8401f9fcd76dfd9d1788d88d0f
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-11-24 00:07:14 -0500

    New node.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,6 +18,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	return (new_node);
 }
