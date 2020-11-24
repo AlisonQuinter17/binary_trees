f32db44786ec95d285358d3eb99b396219f775a8
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-11-24 00:55:38 -0500

    Delete tree.

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree.
+ * @tree: Pointer to the root node of the tree to delete.
+ * Return: Nothing.
+ **/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if(tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
