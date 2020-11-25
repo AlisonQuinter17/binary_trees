db3451517bc900059980299187906e3cc43b8821
Athesto <gamez.live@gmail.com>
2020-11-25 18:56:12 -0500

    leaves solucion ingenua

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - ?
- * @tree: ?
- * Return: ?
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of leaves
+ * Return: number of leaves or 0 if there is no tree
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	(void) tree;
+	size_t leaves;
+
+	if (tree)
+	{
+		if (!tree->left && !tree->right)
+			leaves = 1;
+		if (tree->left && !tree->right)
+			leaves = binary_tree_leaves(tree->left);
+		if (tree->right && !tree->left)
+			leaves = binary_tree_leaves(tree->right);
+		if (tree->right && tree->left)
+			leaves = binary_tree_leaves(tree->left) +
+				binary_tree_leaves(tree->right);
+
+		return (leaves);
+	}
 	return (0);
 }
