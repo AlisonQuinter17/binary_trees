8a70cfce91a52187edaeccc098bea4e10761c5a2
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-26 11:32:24 -0500

    Nodes of the binary Tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: a pointer to the root node of the tree to count the number of nodes
@@ -12,6 +13,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 				binary_tree_nodes(tree->left) +
 				binary_tree_nodes(tree->right));
 	}
-
 	return (0);
 }
