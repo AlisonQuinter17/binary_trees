ff9a92c7b268ab124741516d062fc5625eea65a7
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-25 19:39:02 -0500

    Size of the binary Tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,6 +6,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	(void) tree;
-	return (0);
+	size_t left_size = 0, right_size = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
 }
