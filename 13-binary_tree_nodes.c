37425afe30fa0b7b7703d0853d5d61ca9e9b9a70
Athesto <gamez.live@gmail.com>
2020-11-25 19:31:50 -0500

    13-node solucion sin variables extras

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,13 +6,9 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
-
 	if (tree)
 	{
-		if (tree->left || tree->right)
-			nodes = 1;
-		return (nodes +
+		return ((size_t)(tree->left || tree->right) +
 				binary_tree_nodes(tree->left) +
 				binary_tree_nodes(tree->right));
 	}
