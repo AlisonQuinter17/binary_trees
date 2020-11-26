131bfe8065b76c019b555fa9eb7bcc340e41fd50
Athesto <gamez.live@gmail.com>
2020-11-25 19:00:14 -0500

    leaves solucion final

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,11 +11,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (!tree->left && !tree->right)
-			leaves = 1;
-		if (tree->left)
-			leaves += binary_tree_leaves(tree->left);
-		if (tree->right)
-			leaves += binary_tree_leaves(tree->right);
+			return (1);
+		return (binary_tree_leaves(tree->left) +
+				binary_tree_leaves(tree->right));
 	}
 	return (leaves);
 }
