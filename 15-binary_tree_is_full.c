a76d6167dbb33887589683ebc923f286fbdb77e8
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-26 16:49:20 -0500

    15-if full only one return value

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,11 +1,17 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_full - ?
- * @tree: ?
- * Return: ?
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree:  a pointer to the root node of the tree to check
+ * Return: 0 tree is NULL or not full or 1 if its full
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	(void) tree;
-	return (0);
+		/* !xor: iguales(1), diferentes(0) */
+		return
+		(
+			tree &&
+			!(binary_tree_is_full(tree->left) ^
+			binary_tree_is_full(tree->right))
+		);
 }
